2d26446e800c07db75a67e0ed323c932013cd938
kotaesaki <gurico1229@gmail.com>
2021-01-17 18:47:30 +0900

    props

--- backend/app/Http/Controllers/MypageController.php
+++ backend/app/Http/Controllers/MypageController.php
@@ -3,26 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class MypageController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Auth $auth)
     {
-        return view('mypage');
+        $user = User::all();
+        return $user;
     }
 }
--- backend/resources/js/components/MypageComponent.vue
+++ backend/resources/js/components/MypageComponent.vue
@@ -6,7 +6,8 @@
             <p>人気の投稿</p>
 
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4">  
+            <p>{{id.login_id}}</p>
             <ul id="mypage-item">
                 <li v-for="item in items" :key="item.message">
                     {{ item.message }}
@@ -18,17 +19,31 @@
 </template>
 <script>
 export default {
+    props: {
+        userId: String
+    },
     data() {
         return{
-       items:[
-        {message: '全ての写真'},
-        {message: 'フォロー'},
-        {message: 'フォロワー'},
-        {message: '投稿したタグ'},
-        {message: 'いいねした写真'}
-       ]
+            items:[
+                {message: '全ての写真'},
+                {message: 'フォロー'},
+                {message: 'フォロワー'},
+                {message: '投稿したタグ'},
+                {message: 'いいねした写真'}
+            ],
+            id:{}
        }
-   } 
+   },
+   methods: {
+       getId() {
+           axios.get('/api/mypage' + this.userId).then((res)=>{
+               this.id = res.data;
+           })
+       }
+   }, 
+   mounted() {
+       this.getId();
+   },
     
 }
 </script>
