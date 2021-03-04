386aaddc4feaba41e2a25ca76509f087eeec1779
kotaesaki <gurico1229@gmail.com>
2021-03-05 00:13:53 +0900

    テスト作成

---
+++ backend/tests/Feature/TimelineControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TimelineControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $response = $this->get('/api/timeline');
+        $response->assertStatus(200);
+    }
+    public function testGetTrend()
+    {
+        $response = $this->get('/api/trend_timeline');
+        $response->assertStatus(200);
+    }
+}
