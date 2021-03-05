b2a4f019eaebb41cfe2a46a6361ec97eb8bbae4a
kotaesaki <gurico1229@gmail.com>
2021-03-05 22:59:30 +0900

    circleciテスト

--- backend/config/database.php
+++ backend/config/database.php
@@ -96,7 +96,7 @@ return [
             'port' => '3306',
             'database' => 'laravel_local',
             'username' => 'phper',
-            'password' => '',
+            'password' => 'secret',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
