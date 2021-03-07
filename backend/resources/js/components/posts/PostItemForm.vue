544bc51be62962ed5c415e4205bd51de6f735cf1
kotaesaki <gurico1229@gmail.com>
2021-03-07 17:15:48 +0900

    変になりました!

---
+++ backend/resources/js/components/posts/PostItemForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-group">
+    <div class="contents">
+      <div class="explain">
+        <p class="title">
+          アイテムタグを追加する
+        </p>
+        <p class="required">
+          ※必須
+        </p>
+      </div>
+      <div class="items">
+        <div class="searchBtn">
+          検索し
+        </div>
+        <input
+          id="itemTag"
+          type="text"
+          name="itemTag">
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.searchBtn{
+  border: 1px solid #DEF2FF;
+}
+</style>
